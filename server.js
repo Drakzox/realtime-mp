437bd1a7f9ef5fa26c3e0023c9426622a3e58f33
Drakzox <Orikiyehiel@gmail.com>
2025-08-21 22:52:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
+// serve files from public folder
+app.use(express.static("public"));
+
+// socket.io connection
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // relay chat messages
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  // cleanup
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// start server
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
